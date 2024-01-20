4e4a67c36106ceef1e1c1611308f240cfc93b2f9
Ahmed Mulla <67974888+MullaAhmed@users.noreply.github.com>
2024-01-20 14:39:13 +0530

    Update colab.sh
    deck-generator ----> express-deck

--- express-deck/colab.sh
+++ express-deck/colab.sh
@@ -8,43 +8,43 @@ mkdir templates/styles
 mkdir templates/fonts
 
 # Downloading .py files
-wget -q -P functions https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/functions/__init__.py 
-wget -q -P functions https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/functions/constants.py
-wget -q -P functions https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/functions/content_functions.py
-wget -q -P functions https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/functions/main_functions.py
-wget -q -P functions https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/functions/images_functions.py
+wget -q -P functions https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/functions/__init__.py 
+wget -q -P functions https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/functions/constants.py
+wget -q -P functions https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/functions/content_functions.py
+wget -q -P functions https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/functions/main_functions.py
+wget -q -P functions https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/functions/images_functions.py
 
 # Downloading background images
-wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/backgrounds/bg_img_style10.png
-wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/backgrounds/bg_img_style11.png
-wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/backgrounds/bg_img_style12.png
-wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/backgrounds/bg_img_style13.png
+wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/backgrounds/bg_img_style10.png
+wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/backgrounds/bg_img_style11.png
+wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/backgrounds/bg_img_style12.png
+wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/backgrounds/bg_img_style13.png
 
-wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/backgrounds/bg_img_style20.png
-wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/backgrounds/bg_img_style21.png
-wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/backgrounds/bg_img_style22.png
-wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/backgrounds/bg_img_style23.png
+wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/backgrounds/bg_img_style20.png
+wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/backgrounds/bg_img_style21.png
+wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/backgrounds/bg_img_style22.png
+wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/backgrounds/bg_img_style23.png
 
 
-wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/backgrounds/bg_img_style30.png
-wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/backgrounds/bg_img_style31.png
-wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/backgrounds/bg_img_style32.png
-wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/backgrounds/bg_img_style33.png
+wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/backgrounds/bg_img_style30.png
+wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/backgrounds/bg_img_style31.png
+wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/backgrounds/bg_img_style32.png
+wget -q -P templates/backgrounds https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/backgrounds/bg_img_style33.png
 
 
 # Downloading .html files
-wget -q -P templates/components https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/components/components.html
-wget -q -P templates/components https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/components/template.html
+wget -q -P templates/components https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/components/components.html
+wget -q -P templates/components https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/components/template.html
 
 # Downloading font files
-wget -q -P templates/fonts https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/fonts/BukhariScript-lemD.ttf
-wget -q -P templates/fonts https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/fonts/VaguelyRetroRegular-3zAqM.ttf
+wget -q -P templates/fonts https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/fonts/BukhariScript-lemD.ttf
+wget -q -P templates/fonts https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/fonts/VaguelyRetroRegular-3zAqM.ttf
 
 # Downloading .css files
-wget -q -P templates/styles https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/styles/style1.css
-wget -q -P templates/styles https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/styles/style2.css
-wget -q -P templates/styles https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/styles/style3.css
+wget -q -P templates/styles https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/styles/style1.css
+wget -q -P templates/styles https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/styles/style2.css
+wget -q -P templates/styles https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/styles/style3.css
 
 # Downloading config.json
-wget -q -P templates https://github.com/MullaAhmed/RunAwayML/raw/main/deck-generator/templates/config.json
+wget -q -P templates https://github.com/MullaAhmed/RunAwayML/raw/main/express-deck/templates/config.json
 
